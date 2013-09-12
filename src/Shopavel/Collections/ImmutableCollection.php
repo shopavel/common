fbe713e979182cd719af42b34da92435b01b928f
Laurence Roberts <lsjroberts@gmail.com>
2013-09-13 00:29:13 +0100

    Updated ImmutableCollection to work with IlluminateCollection

--- src/Shopavel/Collections/ImmutableCollection.php
+++ src/Shopavel/Collections/ImmutableCollection.php
@@ -7,14 +7,63 @@
  */
 class ImmutableCollection extends Collection {
 
-    public function add($key, $value)
+    public function put($key, $value)
+    {
+        $this->guard();
+    }
+
+    public function shift()
+    {
+        $this->guard();
+    }
+
+    public function push($value)
+    {
+        $this->guard();
+    }
+
+    public function pop()
+    {
+        $this->guard();
+    }
+
+    public function forget($key)
+    {
+        $this->guard();
+    }
+
+    public function each(Closure $callback)
+    {
+        $this->guard();
+    }
+
+    public function map(Closure $callback)
+    {
+        $this->guard();
+    }
+
+    public function filter(Closure $callback)
+    {
+        $this->guard();
+    }
+
+    public function sort(Closure $callback)
+    {
+        $this->guard();
+    }
+
+    public function sortBy(Closure $callback)
+    {
+        $this->guard();
+    }
+
+    public function reverse()
+    {
+        $this->guard();
+    }
+
+    protected function guard()
     {
         throw new CollectionException("Can not modify an immutable collection.");
     }
-
-    public function remove($key)
-    {
-        throw new CollectionException("Can not modify an immutable collection.");
-    }
-
 }
