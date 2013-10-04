1ae2e231abf136e81bcf051b42a631eda1cec76f
Laurence Roberts <lsjroberts@gmail.com>
2013-10-04 01:39:35 +0100

    Added container trait and moved validators to within transactions

---
+++ src/Shopavel/Collections/Container.php
@@ -0,0 +1,37 @@
+<?php namespace Shopavel\Containers;
+
+/**
+ * A container is an entity which can dynamically contain multiple other
+ * entities within a collection. The contained entities are made available as
+ * properties on the object using the magic __get() method.
+ *
+ * @author Laurence Roberts <lsjroberts@outlook.com>
+ */
+trait Container {
+
+    /**
+     * Add the collection to the container.
+     *
+     * @param Collection $collection
+     */
+    public function setCollection(Collection $collection)
+    {
+        $this->collection = $collection;
+    }
+
+    /**
+     * Look for the property within the collection.
+     *
+     * @param  string $key
+     * @return mixed
+     */
+    public function __get($key)
+    {
+        if ($this->collection->has($key)) {
+            return $this->collection->get($key);
+        }
+
+        return parent::__get($key);
+    }
+
+}
