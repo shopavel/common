9fa6b29d070331da9bb20c290d36f7a24ac27054
Laurence Roberts <lsjroberts@outlook.com>
2014-02-13 23:29:40 +0000

    Renamed collection container to collection collector

--- src/Shopavel/Collections/Collector.php
+++ src/Shopavel/Collections/Collector.php
@@ -1,16 +1,16 @@
 <?php namespace Shopavel\Collections;
 
 /**
- * A container is an entity which can dynamically contain multiple other
- * entities within a collection. The contained entities are made available as
+ * A collector is an entity which can dynamically contain multiple other
+ * entities within a collector. The collected entities are made available as
  * properties on the object using the magic __get() method.
  *
  * @author Laurence Roberts <lsjroberts@outlook.com>
  */
-trait Container {
+trait Collector {
 
     /**
-     * Add the collection to the container.
+     * Add the collection to the collector.
      *
      * @param Collection $collection
      */
@@ -30,8 +30,6 @@ trait Container {
         if ($this->collection->has($key)) {
             return $this->collection->get($key);
         }
-
-        return parent::__get($key);
     }
 
 }
